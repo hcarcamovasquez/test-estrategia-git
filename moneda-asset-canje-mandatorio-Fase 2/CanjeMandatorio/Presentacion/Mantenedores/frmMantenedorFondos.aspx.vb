17bf8425d6a546f5597e1d534299b07f404cd94c
Jonathan Kemp <jkemp@dbsolution.cl>
2021-02-28 22:17:02 -0300

    Correccion Issue 738

--- moneda-asset-canje-mandatorio-Fase 2/CanjeMandatorio/Presentacion/Mantenedores/frmMantenedorFondos.aspx.vb
+++ moneda-asset-canje-mandatorio-Fase 2/CanjeMandatorio/Presentacion/Mantenedores/frmMantenedorFondos.aspx.vb
@@ -430,7 +430,8 @@ Partial Class Presentacion_Mantenedores_frmMantenedorFondos
         txtControlCantidadDias.Enabled = (fondo.ControlTipoDeConfiguracion.Equals("Pago"))
         chkControlDiasHabiles.Enabled = (fondo.ControlTipoDeConfiguracion.Equals("Pago"))
         txtControlDiasAVerificar.Enabled = Not (fondo.ControlTipoControl.Equals("Ventana"))
-
+        txtControlCantidadDias.Enabled = Not (fondo.ControlTipoControl.Equals("Ventana"))
+        chkControlDiasHabiles.Enabled = Not (fondo.ControlTipoControl.Equals("Ventana"))
 
     End Sub
 
