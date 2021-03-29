b4303501ee354652c93fa22f1453af9f32690475
Hernan Reyes <37184591+hareyes@users.noreply.github.com>
2021-03-29 17:24:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ console.log('Hola este es release 2');
 console.log('Hola ete es el release 3, funcionalidad 1')
 
 console.log('Release 3 sin QA, pagina principal.html')
+
+consultar()
+{
+  alert('Hola');
+}
